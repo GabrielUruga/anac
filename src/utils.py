8486be1f0c5cc048c69370bea74def35a8905d90
Gabriel Uruga da Silva <gurugadasilva@gmail.com>
2026-06-27 19:36:04 -0300

    feat: adiciona classe DDL

--- src/utils.py
+++ src/utils.py
@@ -130,3 +130,137 @@ class JSON:
         except Exception as e:
             message = f'{e}'
             logging.error(message, exc_info=True)
+
+class DDL:
+    @staticmethod
+    def create_bigquery_dataset(project_name : str, dataset_name:str, dataset_location:str):
+                  
+        # Definindo o nome do dataset
+        dataset_id = f"{project_name}.{dataset_name}"
+
+        # Definindo instância do Dataset 
+        dataset = bigquery.Dataset(dataset_id)
+
+        # Definindo local de armazenamento do Dataset no Google Cloud
+        dataset.location = dataset_location
+        
+        try:
+            # Verificando se o dataset já existe no projeto 
+            BigQueryClient.get_dataset(dataset_name)
+            
+            # Mensagem para log de sucesso
+            message = f'O dataset `{dataset_name}` já existe no projeto`{project_name}`'
+            logging.info(message)
+
+            return dataset_name
+        
+        except exceptions.NotFound:
+            # Mensagem para log de sucesso
+            message = f'O dataset {dataset_name} não existe no projeto `{project_name}` e será criado'
+            logging.info(message)
+            
+            try:
+                # Instanciando função para criar dataset no projeto do BigQuery
+                create_bigquery_dataset = BigQueryClient.create_dataset(dataset, timeout = 30)
+                
+                # Mensagem para log de sucesso
+                message = f'Dataset `{dataset_name}` criado com sucesso no projeto `{project_name}`' 
+                logging.info(message)
+
+                return dataset_name
+
+            except Exception as e:
+                # Mensagem para log de erro
+                message = f'Erro ao executar criação do dataset `{dataset_name}` no projeto `{project_name}`'
+                logging.error(message, exc_info = True)
+
+    @staticmethod
+    def delete_bigquery_dataset(project_name:str, dataset_name:str):
+        try:
+            # Verificando se o dataset existe no projeto 
+            BigQueryClient.get_dataset(dataset_name)
+            
+            # Mensagem para log caso o dataset não exista no projeto
+            message = f'Não foi possível deletar o dataset `{dataset_name}`, pois o dataset não existe no projeto`{project_name}`'
+            logging.info(message)
+            
+        except exceptions.NotFound:
+            message = f'O dataset `{dataset_name}` existe no projeto `{project_name}` e será deletado'
+            logging.info(message)
+
+            try:
+                # Requisição no Projeto GCP para deletar Dataset e todo seu conteúdo
+                BigQueryClient.delete_dataset(dataset=dataset_name, delete_contents=True)
+
+                # Mensagem de Sucesso após deleção com sucesso do Dataset definido 
+                message = f'Dataset `{dataset_name}` deletado com sucesso do projeto `{project_name}`' 
+                logging.info(message)
+            
+            except Exception as e:
+                # Mensagem para log de erro
+                message = f'Erro ao deletar o dataset `{dataset_name}` do projeto `{project_name}`'
+                logging.error(message, exc_info=True)
+        
+    @staticmethod
+    def create_bigquery_table_by_json(project_name:str, dataset_name:str, url:str, json_file:str):
+        """
+            Método para criar tabela no BigQuery, em dataset e projeto definidos, através de um arquivo JSON
+        """
+        # Importando dependências
+        from urllib.parse import urlparse, unquote
+        from pathlib import PurePosixPath
+        import re
+        
+        # definindo a URL em que o arquivo JSON será extraído
+        json_url = url
+
+        # Extraindo o Path da URL  
+        json_path = PurePosixPath(urlparse(json_url).path)
+        
+        # Extraindo apenas nome do Path da URL removendo espaços e caracteres especiais
+        json_path_name = re.sub(r"\W+", "", unquote(json_path.stem))
+
+        # Definindo o nome do Path da URL como nome da tabela
+        table_name = f'{project_name}.{dataset_name}.{json_path_name}'
+
+        try:
+            # Verificando se tabela já existe
+            BigQueryClient.get_table(table_name)
+
+            # Mensagem para log de tabela já existente
+            message = f'A tabela `{table_name}` não será criada, pois a tabela já existe'
+            logging.info(message)
+            
+            # Retornando nome da tabela, caso ela já exsita
+            return table_name
+        # Excessão para criar tabela, caso ela não exista
+        except exceptions.NotFound:     
+            # Transformar a string decodificada em um dicionário/lista Python
+            json_data = json_file
+
+            # Extraindo as chaves do JSON para utilizar como nome no Schema da tabela
+            keys = json_data[0].keys()          
+
+            # Definindo chaves extraídas do JSON como campos no schema da tabela
+            schema = [bigquery.SchemaField(f"{key}", "STRING", mode="NULLABLE") for key in keys] 
+
+            # Definindo variável com nome e schema da tabela que será criada no BigQuery
+            table = bigquery.Table(table_name, schema=schema,)
+
+            # Requisição para criar tabela definida
+            create_table = BigQueryClient.create_table(table=table)
+            
+            # Bloco Try/Catch para enviar a Requisição para criação da tabela no BigQuery
+            try:
+                create_table
+                
+                # Mensagem para log de sucesso
+                message = f'Tabela `{table_name}` criada com sucesso'
+                logging.info(message)
+                
+                return table_name
+                
+            except Exception:
+                # Mensagem para log de erro 
+                message = f'Erro ao criar tabela `{table_name}`'
+                logging.error(message, exc_info=True)
